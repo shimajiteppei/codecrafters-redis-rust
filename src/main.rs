bf90134a56f91cf85ba583c591ae325391a05570
tomoesaturn <hotarutwitter@gmail.com>
2023-01-09 02:01:51 +0900

    2 Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::TcpListener, io::{self, BufRead, Write}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,17 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
+                let mut reader = io::BufReader::new(&mut _stream);
+                let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+                reader.consume(received.len());
+                println!("{:?}", String::from_utf8(received).unwrap());
+
                 println!("accepted new connection");
+
+                let response = "+PONG\r\n";
+                _stream.write(response.as_bytes()).unwrap();
+                _stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
