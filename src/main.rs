0618df577b4c0e5875a346018af66d45ec649bb6
tomoesaturn <hotarutwitter@gmail.com>
2023-01-09 02:12:02 +0900

    3 Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::{net::TcpListener, io::{self, BufRead, Write}};
+use std::{net::{TcpListener, TcpStream}, io::{self, BufRead, Write}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,20 +12,26 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let mut reader = io::BufReader::new(&mut _stream);
-                let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
-                reader.consume(received.len());
-                println!("{:?}", String::from_utf8(received).unwrap());
-
-                println!("accepted new connection");
-
-                let response = "+PONG\r\n";
-                _stream.write(response.as_bytes()).unwrap();
-                _stream.flush().unwrap();
+                loop {
+                    pingpong(&mut _stream);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn pingpong(mut stream: &TcpStream) {
+    let mut reader = io::BufReader::new(&mut stream);
+    let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+    reader.consume(received.len());
+    println!("{:?}", String::from_utf8(received).unwrap());
+
+    println!("accepted new connection");
+
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
